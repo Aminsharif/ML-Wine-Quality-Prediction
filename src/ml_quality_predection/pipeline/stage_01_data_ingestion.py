61b52e5365f8bf8c7a14664c8e1484561f8425ae
Amin Sharif <33375704+Aminsharif@users.noreply.github.com>
2025-01-23 18:22:07 +0600

    completed

---
+++ src/ml_quality_predection/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,18 @@
+
+from ml_quality_predection.config.configuration import ConfigurationManager
+from ml_quality_predection.components.data_ingestion import DataIngestion
+from ml_quality_predection import logger
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
---
+++ src/ml_quality_predection/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,29 @@
+from ml_quality_predection.config.configuration import ConfigurationManager
+from ml_quality_predection.components.data_transformation import DataTransformation
+from ml_quality_predection import logger
+from pathlib import Path
+
+
+STAGE_NAME = "Data Transformation stage"
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+
+    def main(self):
+        try:
+            with open(Path("artifacts/data_validation/status.txt"), "r") as f:
+                status = f.read().split(" ")[-1]
+
+            if status == "True":
+                config = ConfigurationManager()
+                data_transformation_config = config.get_data_transformation_config()
+                data_transformation = DataTransformation(config=data_transformation_config)
+                data_transformation.train_test_spliting()
+
+            else:
+                raise Exception("You data schema is not valid")
+
+        except Exception as e:
+            print(e)
